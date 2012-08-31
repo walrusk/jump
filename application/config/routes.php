0832c9e4a14a442f6eb8bacd63167e40579cad4e
walrusk <walrusk@gmail.com>
2012-08-30 19:33:31 -0500

    turned of registration

--- application/config/routes.php
+++ application/config/routes.php
@@ -41,6 +41,8 @@
 $route['default_controller'] = "jump";
 $route['404_override'] = '';
 
+$route['login'] = "auth/login";
+$route['logout'] = "auth/logout";
 
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
